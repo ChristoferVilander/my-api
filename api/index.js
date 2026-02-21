2103828034a51ded2dc9f6701f750a83aecf7e71
Christofer Vilander <6317669+ChristoferVilander@users.noreply.github.com>
2026-02-21 20:35:16 +0100

    Delete api/index.js

--- api/index.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "Det här är mitt publika bio-API. Gör en request mot /api/v1/me",
-    endpoints: {
-      me: "/api/v1/me"
-    },
-    example: "curl https://my-api-psi-henna.vercel.app/api/v1/me"
-  });
-}
